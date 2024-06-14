fa578feb1d7f7ed129f8dd6844374292d33184e5
Leandro Capitol <leandrorezendecapitol@gmail.com>
2024-06-14 15:02:04 -0300

    adicao de controllers e imagem inicial

---
+++ CategoriasMVC/CategoriasMVC/Models/CategoriaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CategoriasMVC.Models
+{
+    public class CategoriaViewModel
+    {
+        public int CategoriaId { get; set; }
+        [Required(ErrorMessage = "O nome da categoria é obrigatório")]
+        public string? Nome { get; set; }
+        [Required]
+        [Display(Name ="Imagem")]
+        public string?  ImagemUrl { get; set; }
+    }
+}
